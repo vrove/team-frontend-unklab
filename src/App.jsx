e5a76524683f9b7f15793d95b614ed5dc9dce832
vincents <vianvincent21@gmail.com>
2023-10-19 00:01:07 +0800

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
+import Header from './components/Header/index'
+import Home from './components/Home/index'
+import Footer from './components/Footer/index'
 
 function App() {
   return (
-    <h1>App.js</h1>
+    <div>
+      <h1>App.js</h1>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
   )
 }
 
