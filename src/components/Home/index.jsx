7d71da95fed1d01a469f07abb6a7a8699c62d44f
ezrapelealu <pelealuezra3@gmail.com>
2023-10-18 23:57:45 +0800

    Fix index Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,12 @@
+import CardInfo from './CardInfo/index'
+import Search from './Search/index'
+
 const Home = () => {
     return (
       <div>
         <h1>Home</h1>
+        <CardInfo/>
+        <Search/>
       </div>
     )
   }
