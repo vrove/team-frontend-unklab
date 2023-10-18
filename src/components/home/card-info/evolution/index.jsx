4798889456cdf1143ed8bf93b90a0fb2a40fac43
Vincent Vian Supit <70263114+vrove@users.noreply.github.com>
2023-10-18 09:46:28 +0800

    Delete src/components/home directory

--- src/components/home/card-info/evolution/index.jsx
+++
@@ -1,7 +0,0 @@
-const evolution =  () => {
-    return (
-        <h1>evolution</h1>
-    )
-}
-
-export default evolution
--- src/components/home/index.jsx
+++
@@ -1,7 +0,0 @@
-const home = () => {
-    return (
-        <h1>home</h1>
-    )
-}
-
-export default home
