4ae96ba36d67df525cc4fcd245eb19f0251de792
likuayan <likuayangnazar@gmail.com>
2023-10-18 23:50:09 +0800

    fix file CardInfo

--- src/components/Home/CardInfo/index.jsx
+++ src/components/Home/CardInfo/index.jsx
@@ -1,7 +1,14 @@
+import Evolution from './Evolution/index'
+import Logo from './Logo/index'
+import TypeEffect from './TypeEffect/index'
+
 const CardInfo = () => {
     return (
       <div>
         <h1>CardInfo</h1>
+        <Evolution/>
+        <Logo/>
+        <TypeEffect/>
       </div>
       
     )
